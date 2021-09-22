4bce73e18c305b6a567a95199f86adc43f6ac7ef
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-09-22 14:58:53 -0600

    first commit

---
+++ src/components/Comment.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="comment">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Comment',
+  props: {
+    commentProp: Object
+  },
+  setup (props) {
+    return {
+      comment: props.commentProp
+    }
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
